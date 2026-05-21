a5ec108b8aa9723d4788b9eaccc5a1107659a2a1
Silvia Lin Lü <ssll@kth.se>
2026-05-21 11:51:34 +0200

    Two found real redos, inside folder RESULTS (including links to github)

--- src/main/java/cn/ac/ios/Test.java
+++ src/main/java/cn/ac/ios/Test.java
@@ -32,7 +32,7 @@ import static cn.ac.ios.Utils.Utils.readFile;
 public class Test {
     public static void main(String[] args) throws IOException {
         String filePath = "data/paper_dataset";
-        String fileName = "regex.txt";
+        String fileName = "repo-python.txt";
         run(filePath + "/" + fileName, fileName);
     }
 
